023aa600c95c9ba2cad333dba50b90d082943e09
David Case <david@kronoverse.io>
2021-04-28 18:10:31 -0400

    Fix fyx-purse for OrderLock

--- dist/fyx-purse.js
+++ dist/fyx-purse.js
@@ -12,17 +12,17 @@ class FyxPurse extends run_sdk_1.default.plugins.LocalPurse {
         var _a;
         const tx = bsv_1.Tx.fromHex(rawtx);
         const orderUnlockVout = (_a = parents[0]) === null || _a === void 0 ? void 0 : _a.script.match(order_lock_regex_1.default);
-        if (orderUnlockVout) {
+        if (orderUnlockVout && tx.txOuts[0].script.isSafeDataOut()) {
             tx.addTxIn(tx.txIns[0].txHashBuf, 0, bsv_1.Script.fromString('OP_0 OP_0'), 2 ** 32 - 1);
-            const txid = Buffer.from(tx.txIns[0].txHashBuf).reverse().toString('hex');
-            const lockRawTx = await this.blockchain.fetch(txid);
-            const lockTx = bsv_1.Tx.fromString(lockRawTx);
-            rawtx = tx.toHex();
-            parents.push({
-                script: lockTx.txOuts[0].script.toHex(),
-                satoshis: lockTx.txOuts[0].valueBn.toNumber()
-            });
-            // return tx.toHex();
+            // const txid = Buffer.from(tx.txIns[0].txHashBuf).reverse().toString('hex');
+            // const lockRawTx = await this.blockchain.fetch(txid);
+            // const lockTx = Tx.fromString(lockRawTx);
+            // rawtx = tx.toHex();
+            // parents.push({
+            //     script: lockTx.txOuts[0].script.toHex(),
+            //     satoshis: lockTx.txOuts[0].valueBn.toNumber()
+            // })
+            return tx.toHex();
         }
         ;
         return super.pay(rawtx, parents);
--- src/fyx-purse.ts
+++ src/fyx-purse.ts
@@ -7,17 +7,17 @@ export class FyxPurse extends Run.plugins.LocalPurse {
     async pay(rawtx: string, parents: { satoshis: number, script: string }[]) {
         const tx = Tx.fromHex(rawtx);
         const orderUnlockVout = parents[0]?.script.match(orderLockRegex);
-        if(orderUnlockVout) {
+        if(orderUnlockVout && tx.txOuts[0].script.isSafeDataOut()) {
             tx.addTxIn(tx.txIns[0].txHashBuf, 0, Script.fromString('OP_0 OP_0'), 2**32-1);
-            const txid = Buffer.from(tx.txIns[0].txHashBuf).reverse().toString('hex');
-            const lockRawTx = await this.blockchain.fetch(txid);
-            const lockTx = Tx.fromString(lockRawTx);
-            rawtx = tx.toHex();
-            parents.push({
-                script: lockTx.txOuts[0].script.toHex(),
-                satoshis: lockTx.txOuts[0].valueBn.toNumber()
-            })
-            // return tx.toHex();
+            // const txid = Buffer.from(tx.txIns[0].txHashBuf).reverse().toString('hex');
+            // const lockRawTx = await this.blockchain.fetch(txid);
+            // const lockTx = Tx.fromString(lockRawTx);
+            // rawtx = tx.toHex();
+            // parents.push({
+            //     script: lockTx.txOuts[0].script.toHex(),
+            //     satoshis: lockTx.txOuts[0].valueBn.toNumber()
+            // })
+            return tx.toHex();
         };
         return super.pay(rawtx, parents);
     }
