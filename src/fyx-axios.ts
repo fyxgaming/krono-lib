d80e3c6fd024d0f5ceca297a9a1aca295b174070
David Case <david@fyxgaming.com>
2021-10-17 00:26:54 -0400

    Remove unused dependencies

--- src/fyx-axios.ts
+++ src/fyx-axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
-// import * as rax from 'retry-axios';
 import createError from 'http-errors';
 
-// rax.attach();
 axios.interceptors.response.use((r) => r, (e) => {
     if(e.response) throw createError(e.response.status, `${e.config.url} -  ${JSON.stringify(e.response.data)}`)
     throw e;
