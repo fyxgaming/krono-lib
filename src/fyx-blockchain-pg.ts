7cf5b61970316507600542ca6b57894fee11f35e
Ashish Chandra <ashish.cppdev@gmail.com>
2021-10-28 10:46:06 -0500

    Added logging to broadcast

--- src/fyx-blockchain-pg.ts
+++ src/fyx-blockchain-pg.ts
@@ -45,7 +45,7 @@ export class FyxBlockchainPg implements IBlockchain {
             FROM derivations
             WHERE pubkey IN (${pubkeys.length ? pubkeys : Buffer.from('00', 'hex')}) 
                 OR script IN (${outScripts.length ? outScripts : Buffer.from('00', 'hex')})`;
-        if(!derivations.length) {
+        if (!derivations.length) {
             console.log('No pubkeys or scripts:', txid);
             throw new createError.NotFound();
         }
@@ -57,74 +57,110 @@ export class FyxBlockchainPg implements IBlockchain {
             pubkeysPaths.set(d.pubkey, d.path);
             scriptPaths.set(d.script, d.path);
         });
-
+        // logging
+        console.log(`pubkeysPaths: ${JSON.stringify(pubkeysPaths, null, 4)}`);
+        console.log(`scriptPaths: ${JSON.stringify(scriptPaths, null, 4)}`);
+        // end logging
         const fundSpends = [];
         const jigSpends = [];
         const marketSpends = [];
-        await Promise.all(tx.txIns.map(async (t, vin) => {
-            const pubkey = t.script.isPubKeyHashIn() && t.script.chunks[1].buf.toString('hex');
-            const path = pubkeysPaths.get(pubkey);
-            const spend = {
-                txid: Buffer.from(t.txHashBuf).reverse(),
-                vout: t.txOutNum
-            }
-            if(path === 'm/0/0') {
-                const [spent] = await this.sql`SELECT encode(spend_txid, 'hex') as spend_txid
+        console.log(`Entering Promise.all for spends...`);
+        try {
+            await Promise.all(tx.txIns.map(async (t, vin) => {
+                const pubkey = t.script.isPubKeyHashIn() && t.script.chunks[1].buf.toString('hex');
+                const path = pubkeysPaths.get(pubkey);
+                const spend = {
+                    txid: Buffer.from(t.txHashBuf).reverse(),
+                    vout: t.txOutNum
+                }
+                // console.log(`pubkey: ${JSON.stringify(pubkey)}`);
+                // console.log(`path: ${path}`);
+                // console.log(`spend: ${JSON.stringify(spend)}`);
+                if (path === 'm/0/0') {
+                    const [spent] = await this.sql`SELECT encode(spend_txid, 'hex') as spend_txid
                     FROM fund_txos_spent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}`;
-                if(spent && spent.spend_txid !== txid) {
-                    throw createError(422, `Input already spent: ${txid} - ${spend.txid.toString('hex')} ${spend.vout}`);
-                }
-                fundSpends.push(spend);
-            } else if(path) {
-                const [spent] = await this.sql`SELECT encode(spend_txid, 'hex') as spend_txid
+                    if (spent && spent.spend_txid !== txid) {
+                        throw createError(422, `Input already spent: ${txid} - ${spend.txid.toString('hex')} ${spend.vout}`);
+                    }
+                    // console.log(`In path m/0/0 - retrieved spent ${JSON.stringify(spent)}`);
+                    fundSpends.push(spend);
+                    // console.log(`Added spend to fundSpends`);
+                } else if (path) {
+                    const [spent] = await this.sql`SELECT encode(spend_txid, 'hex') as spend_txid
                     FROM jig_txos_spent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}`;
-                if(spent && spent.spend_txid !== txid) {
-                    throw createError(422, `Input already spent: ${txid} - ${spend.txid.toString('hex')} ${spend.vout}`);
-                }
-                jigSpends.push(spend);
-            } else if(t.script.toHex().match(orderLockRegex)){
-                const [spent] = await this.sql`SELECT encode(spend_txid, 'hex') as spend_txid
+                    if (spent && spent.spend_txid !== txid) {
+                        throw createError(422, `Input already spent: ${txid} - ${spend.txid.toString('hex')} ${spend.vout}`);
+                    }
+                    // console.log(`In path ${path} - retrieved spent ${JSON.stringify(spent)}`);
+                    jigSpends.push(spend);
+                    // console.log(`Added spend to jigSpends`);
+                } else if (t.script.toHex().match(orderLockRegex)) {
+                    const [spent] = await this.sql`SELECT encode(spend_txid, 'hex') as spend_txid
                     FROM market_txos_spent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}`;
-                if(spent && spent.spend_txid !== txid) {
-                    throw createError(422, `Input already spent: ${txid} - ${spend.txid.toString('hex')} ${spend.vout}`);
+                    if (spent && spent.spend_txid !== txid) {
+                        throw createError(422, `Input already spent: ${txid} - ${spend.txid.toString('hex')} ${spend.vout}`);
+                    }
+                    // console.log(`In condition path not set - retrieved spent ${JSON.stringify(spent)}`);
+                    // console.log(`t.script.toHex().match(orderLockRegex)) - ${JSON.stringify(t.script.toHex().match(orderLockRegex))}`);
+                    marketSpends.push(spend);
+                    // console.log(`Added spend to marketSpends`);
                 }
-                marketSpends.push(spend);
-            }
-        }));
+            }));
+        }
+        catch (e: any) {
+            console.error(`Error from spends Promise.all code block - ${e.message}`);
+            throw createError(422, e.message);
+        }
+        // logging
+        console.log(`Done with Promise.all for spends...`);
+        // if (fundSpends.length > 0) console.log(`fundSpends array is ${JSON.stringify(fundSpends, null, 4)}`);
+        // if (jigSpends.length > 0) console.log(`jigSpends array is ${JSON.stringify(jigSpends, null, 4)}`);
+        // if (marketSpends.length > 0) console.log(`marketSpends array is ${JSON.stringify(marketSpends, null, 4)}`);
+        // end logging
 
         const fundUtxos = [];
         const jigUtxos = [];
         const marketUtxos = [];
-        await Promise.all(tx.txOuts.map(async (t: any, vout: number) => {
-            if (t.script.isSafeDataOut()) return;
-            const script = t.script.toHex();
-            if(t.script.isPubKeyHashOut()) {
-                const path = scriptPaths.get(script);
-                if(path === 'm/0/0') {
-                    fundUtxos.push({
+        console.log(`Entering Promise.all for utxos...`);
+        try {
+            await Promise.all(tx.txOuts.map(async (t: any, vout: number) => {
+                if (t.script.isSafeDataOut()) return;
+                const script = t.script.toHex();
+                if (t.script.isPubKeyHashOut()) {
+                    const path = scriptPaths.get(script);
+                    if (path === 'm/0/0') {
+                        fundUtxos.push({
+                            txid: txidBuf,
+                            vout,
+                            scripthash: (await Hash.asyncSha256(t.script.toBuffer())).reverse(),
+                            satoshis: t.valueBn.toNumber(),
+                        });
+                    } else if (path) {
+                        jigUtxos.push({
+                            txid: txidBuf,
+                            vout,
+                            scripthash: (await Hash.asyncSha256(t.script.toBuffer())).reverse(),
+                            satoshis: t.valueBn.toNumber(),
+                        });
+                    }
+                } else if (t.script.toHex().match(orderLockRegex)) {
+                    marketUtxos.push({
                         txid: txidBuf,
-                        vout,
-                        scripthash: (await Hash.asyncSha256(t.script.toBuffer())).reverse(),
-                        satoshis: t.valueBn.toNumber(),
-                    });
-                } else if(path) {
-                    jigUtxos.push({
-                        txid: txidBuf,
-                        vout,
-                        scripthash: (await Hash.asyncSha256(t.script.toBuffer())).reverse(),
-                        satoshis: t.valueBn.toNumber(),
-                    });
+                        vout
+                    })
                 }
-            } else if(t.script.toHex().match(orderLockRegex)){
-                marketUtxos.push({
-                    txid: txidBuf,
-                    vout
-                })
-            }
-        }));
+            }));
+        }
+        catch (e: any) {
+            console.error(`Error from utxos Promise.all code block - ${e.message}`);
+            throw createError(422, e.message);
+        }
+        // logging
+        console.log(`Done with Promise.all for utxos...`);
+        // end logging
 
         if (BLOCKCHAIN_BUCKET) {
             await this.aws?.s3.putObject({
@@ -136,6 +172,7 @@ export class FyxBlockchainPg implements IBlockchain {
 
         // Broadcast transaction
         let response;
+        console.log(`Broadcasting transaction...`);
         if (MAPI) {
             let resp, axiosInstance = axios.create({ withCredentials: true });
             let cookie = JSON.parse((await this.redis.get(`taal-elb-cookie`)) || '[]');
@@ -188,6 +225,7 @@ export class FyxBlockchainPg implements IBlockchain {
         } else {
             try {
                 await this.rpcClient.sendRawTransaction(rawtx);
+                console.log(`MAPI Not Set - Successfully broadcasted transaction.`);
             } catch (e: any) {
                 if (e.message.includes('Transaction already known') || e.message.includes('Transaction already in the mempool')) {
                     console.log(`Error from sendRawTransaction: ${e.message}`);
@@ -197,69 +235,85 @@ export class FyxBlockchainPg implements IBlockchain {
             }
         }
 
-        await this.sql`INSERT INTO txns(txid) VALUES(${txidBuf}) ON CONFLICT DO NOTHING`,
-        console.log('TX Updates:', txid, JSON.stringify({
-            spends: {
-                fund: fundSpends.map(s => ({txid: s.txid.toString('hex'), vout: s.vout})),
-                jig: jigSpends.map(s => ({txid: s.txid.toString('hex'), vout: s.vout})),
-                market: marketSpends.map(s => ({txid: s.txid.toString('hex'), vout: s.vout}))
-            },
-            txos: {
-                fund: fundUtxos.map(s => ({txid, vout: s.vout})),
-                jig: jigUtxos.map(s => ({txid, vout: s.vout})),
-                market: marketUtxos.map(s => ({txid, vout: s.vout}))
-            }
-        }));
-        await this.sql.begin(async sql => {
-            for(let spend of fundSpends) {
-                await sql`INSERT INTO fund_txos_spent(txid, vout, scripthash, satoshis, spend_txid)
+        console.log(`Inserting data into database`);
+        try {
+            await this.sql`INSERT INTO txns(txid) VALUES(${txidBuf}) ON CONFLICT DO NOTHING`,
+                console.log('TX Updates:', txid, JSON.stringify({
+                    spends: {
+                        fund: fundSpends.map(s => ({ txid: s.txid.toString('hex'), vout: s.vout })),
+                        jig: jigSpends.map(s => ({ txid: s.txid.toString('hex'), vout: s.vout })),
+                        market: marketSpends.map(s => ({ txid: s.txid.toString('hex'), vout: s.vout }))
+                    },
+                    txos: {
+                        fund: fundUtxos.map(s => ({ txid, vout: s.vout })),
+                        jig: jigUtxos.map(s => ({ txid, vout: s.vout })),
+                        market: marketUtxos.map(s => ({ txid, vout: s.vout }))
+                    }
+                }));
+            await this.sql.begin(async sql => {
+                for (let spend of fundSpends) {
+                    await sql`INSERT INTO fund_txos_spent(txid, vout, scripthash, satoshis, spend_txid)
                     SELECT txid, vout, scripthash, satoshis, ${txidBuf} as spend_txid
                     FROM fund_txos_unspent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}
                     ON CONFLICT DO NOTHING`;
-                await sql`DELETE FROM fund_txos_unspent
+                    await sql`DELETE FROM fund_txos_unspent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}`;
-            }
-            if(fundUtxos.length) {
-                await sql`INSERT INTO fund_txos_unspent 
+                }
+                if (fundUtxos.length) {
+                    await sql`INSERT INTO fund_txos_unspent 
                     ${sql(fundUtxos, 'txid', 'vout', 'scripthash', 'satoshis')}
                     ON CONFLICT DO NOTHING`;
-            }
+                }
 
-            for(let spend of jigSpends) {
-                await sql`INSERT INTO jig_txos_spent(txid, vout, scripthash, satoshis, origin, kind, type, spend_txid)
+                for (let spend of jigSpends) {
+                    await sql`INSERT INTO jig_txos_spent(txid, vout, scripthash, satoshis, origin, kind, type, spend_txid)
                     SELECT txid, vout, scripthash, satoshis, origin, kind, type, ${txidBuf} as spend_txid
                     FROM jig_txos_unspent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}
                     ON CONFLICT DO NOTHING`;
-                await sql`DELETE FROM jig_txos_unspent
+                    await sql`DELETE FROM jig_txos_unspent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}`;
-            }
-            if(jigUtxos.length) {
-                await sql`INSERT INTO jig_txos_unspent 
+                }
+                if (jigUtxos.length) {
+                    await sql`INSERT INTO jig_txos_unspent 
                     ${sql(jigUtxos, 'txid', 'vout', 'scripthash', 'satoshis')}
                     ON CONFLICT DO NOTHING`;
-            }
+                }
 
-            for(let spend of marketSpends) {
-                await sql`INSERT INTO market_txos_spent(txid, vout, origin, user_id, spend_txid)
+                for (let spend of marketSpends) {
+                    await sql`INSERT INTO market_txos_spent(txid, vout, origin, user_id, spend_txid)
                     SELECT txid, vout, origin, user_id, ${txidBuf} as spend_txid
                     FROM market_txos_unspent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}
                     ON CONFLICT DO NOTHING`;
-                await sql`DELETE FROM market_txos_unspent
+                    await sql`DELETE FROM market_txos_unspent
                     WHERE txid=${spend.txid} AND vout=${spend.vout}`;
-            }
-            if(marketUtxos.length) {
-                await sql`INSERT INTO market_txos_unspent 
+                }
+                if (marketUtxos.length) {
+                    await sql`INSERT INTO market_txos_unspent 
                     ${sql(marketUtxos, 'txid', 'vout')}
                     ON CONFLICT DO NOTHING`;
-            }
-        });
-        await Promise.all([
-            this.cache.set(`tx://${txid}`, rawtx),
-            this.redis.publish('txn', txid)
-        ]);
+                }
+            });
+        }
+        catch (e: any) {
+            console.error(`Error from Insert into DB code block - ${e.message}`);
+            throw createError(422, e.message);
+        }
+
+        console.log(`Setting Redis cache`);
+
+        try {
+            await Promise.all([
+                this.cache.set(`tx://${txid}`, rawtx),
+                this.redis.publish('txn', txid)
+            ]);
+        }
+        catch (e: any) {
+            console.error(`Error from set Redis cache code block - ${e.message}`);
+            throw createError(422, e.message);
+        }
 
         const isRun = tx.txOuts.find(txOut => {
             return txOut.script.chunks.length > 5 &&
@@ -282,6 +336,7 @@ export class FyxBlockchainPg implements IBlockchain {
                 MessageBody: JSON.stringify({ txid })
             }).promise();
         }
+        console.log(`All processes complete. Returning txid ${txid}`);
         return txid;
     }
 
@@ -309,7 +364,7 @@ export class FyxBlockchainPg implements IBlockchain {
                 if (DEBUG) console.log('Loaded from s3:', txid);
             }
         }
-        
+
         if (!rawtx) {
             console.log('Fallback to WoC Public', txid);
             const { data } = await axios(
@@ -332,7 +387,7 @@ export class FyxBlockchainPg implements IBlockchain {
 
     async calculateScriptHash(owner: string, ownerType: string): Promise<Buffer> {
         let script;
-        if(ownerType === 'scripthash') {
+        if (ownerType === 'scripthash') {
             return Buffer.from(owner, 'hex');
         } else if (ownerType === 'address') {
             const address = Address.fromString(owner);
@@ -343,7 +398,7 @@ export class FyxBlockchainPg implements IBlockchain {
             throw new Error('Invalid ownerType');
         }
         return (await Hash.asyncSha256(script.toBuffer())).reverse();
-        
+
     }
 
     async utxos(owner: string, ownerType = 'script', limit = 1000) {
@@ -416,7 +471,7 @@ export class FyxBlockchainPg implements IBlockchain {
         console.log('UTXO Selected:', scripthash.toString('hex'), JSON.stringify(utxo));
         return utxo;
     }
-    
+
     async applyPayment(tx, payment: { from: string, amount: number }, change = true) {
         const address = Address.fromString(payment.from);
         const scripthash = (await Hash.asyncSha256(address.toTxOutScript().toBuffer())).reverse();
